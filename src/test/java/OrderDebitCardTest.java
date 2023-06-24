b8be3a5f4862917ccd819aede15adf4b312691fe
Alexander <alex.albasov@gmail.com>
2023-06-24 20:11:44 +0700

    Task 1 has been done

--- src/test/java/OrderDebitCardTest.java
+++ src/test/java/OrderDebitCardTest.java
@@ -1,3 +1,19 @@
-public class OrderDebitCardTest {
+import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OrderDebitCardTest {
+    @Test
+    void shouldOrderCardIfDateCorrect() {
+        open("http://localhost:9999/");
+        $("[data-test-id='name'] input").setValue("Иван Антонов");
+        $("[data-test-id='phone'] input").setValue("+79131345555");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $(withText("Ваша заявка успешно отправлена!")).shouldBe(visible, Duration.ofMillis(5000));
+    }
 }
